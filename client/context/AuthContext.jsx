6bac10f7a15eeff35501e010c44da00d7a55fca7
bismay70 <samalbismay5@gmail.com>
2025-11-04 23:12:50 +0530

    auth

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -1,12 +1,27 @@
-import { createContext } from "react";
-
+import { createContext,useState } from "react";
+import axios from "axios";
 export const AuthContext = createContext();
 
 const backendUrl = import.meta.env.VITE_BACKENDUrl;
+axios.default.baseURL=backendUrl;
+
+export const AuthContext = createContext(); 
 
 export const AuthContextProvider = ({children})=>{
-    const value = {
+    const [token,sdetToken] = useState(localStorage.getItem("token") );
+    const [authUser,setAuthUser] = useState(null);
+    const [onlineUsers,setOnlineUsers] = useState([]);
+    const [socket,setSocket] = useState(null);
+
+    const checkAuth = async()=>{
         
+    }
+
+    const value = {
+        axios,
+        authUser,
+        onlineUsers,
+        socket
     }
     return(
         <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
